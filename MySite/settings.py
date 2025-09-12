0f54cdb8d32afa3c0013f781ed84e9f6d1ac0e96
Shcoder <navigatordj201@gmail.com>
2025-09-12 18:48:52 +0300

    Update data

--- MySite/settings.py
+++ MySite/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+# Директорія для зібраної статики
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# Шлях для доступу до статики
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
